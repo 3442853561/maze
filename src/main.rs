126ba7983df614b7333b3c717031bdb144f0e64c
3442853561 <21147967+3442853561@users.noreply.github.com>
2019-04-25 18:33:34 +0800

    19.4.25.18.33

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 extern crate png;
+use png::HasParameters;
 
 mod rand;
-
 use rand::rand;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
 
 fn main(){
     let mut seed = 1;
-    println!("{}", rand(&mut seed));
+    
+    let path = Path::new(r"./image.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, 1500, 1500); 
+    encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
+    
+    let mut writer = encoder.write_header().unwrap();
+    let mut data = vec![255; 1500 * 1500 * 3];
+    
+    // TODO
+    
+    writer.write_image_data(&data).unwrap();
+    
     println!("{}", rand(&mut seed));
     println!("Done!");
 }
