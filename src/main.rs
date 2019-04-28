8263f6f1e06fb8173249b8729e54bde714c04ee9
3442853561 <21147967+3442853561@users.noreply.github.com>
2019-04-28 16:28:12 +0800

    19.4.28.16.28

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 extern crate png;
 use png::HasParameters;
 
+extern crate failure;
+use failure::Error;
+
 mod rand;
 use rand::rand;
 
 use std::path::Path;
 use std::fs::File;
 use std::io::BufWriter;
+ 
+fn main() -> Result<(), Error>{
+    for i in 0..100 {
+        let mut seed = i;
+        let path_str = format!("./image_{}.png", i);
+        let path = Path::new(&path_str);
+        let file = File::create(path)?;
+        let ref mut w = BufWriter::new(file);
 
-fn main(){
-    let mut seed = 1;
-    
-    let path = Path::new(r"./image.png");
-    let file = File::create(path).unwrap();
-    let ref mut w = BufWriter::new(file);
-
-    let mut encoder = png::Encoder::new(w, 1500, 1500); 
-    encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
-    
-    let mut writer = encoder.write_header().unwrap();
-    let mut data = vec![255; 1500 * 1500 * 3];
-    
-    // TODO
-    
-    writer.write_image_data(&data).unwrap();
-    
-    println!("{}", rand(&mut seed));
+        let mut encoder = png::Encoder::new(w, 1500, 1500); 
+        encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
+        
+        let mut writer = encoder.write_header()?;
+        let mut data = vec![255; 1500 * 1500 * 3];
+        
+        
+        
+        writer.write_chunk([112, 72, 89, 115], &[0, 0, 46, 36, 0, 0, 46, 36, 1])?;
+        writer.write_image_data(&data)?;
+        println!("{}: {}", i, rand(&mut seed));
+    }
     println!("Done!");
+    Ok(())
 }
